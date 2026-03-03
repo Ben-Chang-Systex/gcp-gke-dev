32c20d5a051032d384a33d4b21d54fccaa0968db
Ben-Chang-Systex <ben_chang@xsolutions.com.tw>
2026-03-03 15:32:05 +0800

    5

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ data "google_client_config" "default" {}
 # 讀取現有的 GKE 叢集資訊
 data "google_container_cluster" "my_cluster" {
   name     = var.cluster_name
-  location = var.zone
+  location = var.region
   project  = var.project_id
 }
 
